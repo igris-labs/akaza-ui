1c3048c06c563002b994c84f2ca43e10cbc15d37
Rahul Vashishtha <rahulvashistha96@gmail.com>
2026-03-30 14:50:10 +0530

    docs: Fix z-index on component previewer and code

--- docs/app/components/ComponentPreview.vue
+++ docs/app/components/ComponentPreview.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="not-prose relative my-0 rounded-t-xl border border-b-0 border-neutral-200 dark:border-neutral-800">
-    <div class="flex min-h-52 items-center justify-center rounded-t-xl bg-neutral-50 p-8 dark:bg-neutral-950">
+  <div
+    class="not-prose relative my-0 rounded-t-xl border border-b-0 border-neutral-200 dark:border-neutral-800 z-49"
+  >
+    <div
+      class="flex min-h-52 items-center justify-center rounded-t-xl bg-neutral-50 p-8 dark:bg-neutral-950 z-49"
+    >
       <slot />
     </div>
   </div>
